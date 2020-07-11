08a5be9d407e325b184636aaae2cd2cdb217488e
Lesly Tello <lesly.tello@epn.edu.ec>
2020-07-11 15:34:56 -0500

    Uso de class-validator y cookie insegura

--- 01-http/introduccion/src/http/http-juego.controller.ts
+++ 01-http/introduccion/src/http/http-juego.controller.ts
@@ -1,4 +1,19 @@
-import {BadRequestException, Body, Controller, Delete, Get, Header, HttpCode, Param, Post, Query} from "@nestjs/common";
+import {
+    BadRequestException,
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Header,
+    HttpCode,
+    Param,
+    Post,
+    Query,
+    Req, Res
+} from "@nestjs/common";
+import {MascotaCreateDto} from "./dto/mascota.create-dto";
+import {validate, ValidationError} from "class-validator";
+
 
 //Escucha las peticiones pero de que /juegos-http Prefijo a cada controlador
 //http://localhost:3001/juegos-http
@@ -61,12 +76,57 @@ export class HttpJuegoController{
     }
 
     @Post('parametros-cuerpo')
-    parametrosCuerpo(
-       @Body() parametrosDeCuerpo)
-    {
-        console.log('Parametros de cuerpo', parametrosDeCuerpo)
-        const res=parametrosDeCuerpo.mascota.edad + parametrosDeCuerpo.mascota.peso
-        return 'Registro creado'
+    @HttpCode(200)
+    async parametrosCuerpo(
+       @Body() parametrosDeCuerpo
+    ){
+        //promesa
+        const mascotaValida=new MascotaCreateDto();
+        mascotaValida.nombre=parametrosDeCuerpo.nombre;
+        mascotaValida.casada=parametrosDeCuerpo.casada;
+        mascotaValida.edad=parametrosDeCuerpo.edad;
+        mascotaValida.ligada=parametrosDeCuerpo.ligada;
+        mascotaValida.peso=parametrosDeCuerpo.peso;
+        try{
+            const errores:ValidationError[]=await validate(mascotaValida)
+            if(errores.length>0){
+                console.error('Errores',errores)
+                throw new BadRequestException("Error validando")
 
+            }else{
+                return{
+                    mensaje:'Se ha creado correctamente'
+                }
+            }
+        }catch(e){
+            console.error('Error',e);
+            throw new BadRequestException("Error validando");
+
+        }
+
+       /* console.log('Parametros de cuerpo', parametrosDeCuerpo)
+        const res=parametrosDeCuerpo.mascota.edad + parametrosDeCuerpo.mascota.peso
+        return 'Registro creado'*/
+
+    }
+
+
+    //1Guardar una cookie insegura
+    //2 guardar una cookie segura
+    //3 mostrar cookie
+
+    @Get("guardarCookieInsegura")
+    guardarCookieInsegura(
+        @Query() parametrosConsulta,
+        @Req() req, //request-peticion
+        @Res() res //Responde-respuesta
+    ){
+        res.cookie(
+            'galletaInsegura', //nombre
+            'Tengo hambre' //valor
+        )
+        res.send({
+            mensaje: 'ok'
+        });
     }
 }
--- 01-http/introduccion/src/main.ts
+++ 01-http/introduccion/src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app.module'; //En TS
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cookieParser= require('cookie-parser'); //Importar cosas en JS
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -7,6 +9,10 @@ async function bootstrap() {
   * AQUI CONFIGURACION
   * *
    */
+  app.use(cookieParser())
+
   await app.listen(3001);
 }
 bootstrap();
+
+
