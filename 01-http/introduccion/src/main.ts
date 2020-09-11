e729bb645d11867e87d83b1fc02f81172b3043f7
Lesly Tello <lesly.tello@epn.edu.ec>
2020-09-11 16:17:34 -0500

    busqueda y configuracion sesion

--- 01-http/introduccion/src/main.ts
+++ 01-http/introduccion/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module'; //En TS
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const cookieParser= require('cookie-parser'); //Importar cosas en JS
 const express=require('express');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule) as any;
   /*
@@ -12,6 +14,17 @@ async function bootstrap() {
   app.use(cookieParser('Me gusta las poliburguers'))
   app.set('view engine','ejs')
   app.use(express.static('publico'))
+  app.use(
+      session({
+        name: 'server-session-id',
+        secret: 'No sera de tomar un traguito',
+        resave: true,
+        saveUninitialized: true,
+        cookie: {secure: false},
+        store: new FileStore(),
+      }),
+  );
+
 
   await app.listen(3001);
 }
